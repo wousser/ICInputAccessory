48ca61f63e9327a518a114bf5d03652c3eb1637d
Ben <b.cylin@gmail.com>
2017-08-16 23:56:44 +0800

    Reenable UI tests

--- ICInputAccessoryUITests/ICTokenFieldUITests.swift
+++ ICInputAccessoryUITests/ICTokenFieldUITests.swift
@@ -30,15 +30,8 @@ class ICTokenFieldUITests: XCTestCase {
 
   override func setUp() {
     super.setUp()
-    // In UI tests it is usually best to stop immediately when a failure occurs.
     continueAfterFailure = false
-    // UI tests must launch the application that they test. Doing this in setup will make sure it happens for each test method.
     XCUIApplication().launch()
-  }
-
-  override func tearDown() {
-    // Put teardown code here. This method is called after the invocation of each test method in the class.
-    super.tearDown()
   }
 
   func testTokenField() {

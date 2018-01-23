97ff8b11068f6c1a04d93ae95169c8e17f9fb263
Ben <b.cylin@gmail.com>
2018-01-23 17:09:59 +0800

    Skip documentation updates from pull request builds

--- scripts/update-docs.sh
+++ scripts/update-docs.sh
@@ -17,13 +17,14 @@ git --no-pager diff --stat
 git add .
 git commit -m "[CI] Update documentation at $(date +'%Y-%m-%d %H:%M:%S %z')"
 
-if [ "${TRAVIS_BRANCH}" = "develop" ] && [ -n "$DANGER_GITHUB_API_TOKEN" ]; then
+if [ "${TRAVIS_COMMIT_MESSAGE}" = Merge* ] && [ -n "$DANGER_GITHUB_API_TOKEN" ]; then
   echo "Updating gh-pages..."
   git remote add upstream "https://${DANGER_GITHUB_API_TOKEN}@github.com/polydice/ICInputAccessory.git"
   git push --quiet upstream HEAD:gh-pages
   git remote remove upstream
 else
-  echo "Skip gh-pages updates on ${TRAVIS_BRANCH}"
+  echo "Skip gh-pages updates on ${TRAVIS_BRANCH}:"
+  echo "\n    ${TRAVIS_COMMIT_MESSAGE}\n"
 fi
 
 cd -

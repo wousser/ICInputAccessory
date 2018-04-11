8cb56bf777de507196565a178bd01e5ffa0ddc5c
Ben <b.cylin@gmail.com>
2018-04-11 23:02:59 +0800

    Update documentation only on master branch

--- scripts/update-docs.sh
+++ scripts/update-docs.sh
@@ -17,7 +17,7 @@ git --no-pager diff --stat
 git add .
 git commit -m "[CI] Update documentation at $(date +'%Y-%m-%d %H:%M:%S %z')"
 
-if [ "${TRAVIS_COMMIT_MESSAGE}" = Merge* ] && [ -n "$DANGER_GITHUB_API_TOKEN" ]; then
+if [ "${TRAVIS_COMMIT_MESSAGE}" = Merge* ] && [ "${TRAVIS_BRANCH}" = "master" ] && [ -n "$DANGER_GITHUB_API_TOKEN" ]; then
   echo "Updating gh-pages..."
   git remote add upstream "https://${DANGER_GITHUB_API_TOKEN}@github.com/polydice/ICInputAccessory.git"
   git push --quiet upstream HEAD:gh-pages
